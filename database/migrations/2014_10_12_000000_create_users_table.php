dbbc00e2f71af5e8e36135681785e31568e0508c
Asad-Shoaib1 <asadshoaib93@gmail.com>
2023-12-22 18:33:36 +0500

    Added default values

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('repeatpassword');
+            $table->string('repeatpassword')->nullable();
             $table->string('user_type')->default('user')->nullable();
             $table->rememberToken();
             $table->timestamps();
